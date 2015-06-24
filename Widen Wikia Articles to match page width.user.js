a1c8fc5476d90fe8948ab3381fd49e58e97b01c5
SordidFellow <xjc599yddv@snkmail.com>
2015-06-24 14:27:32 -0500

    Add source url for auto-update support

--- Widen Wikia Articles to match page width.user.js
+++ Widen Wikia Articles to match page width.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Widen Wikia Articles to match page width
 // @namespace    http://ogletree.io/
-// @version      0.1
+// @version      1.0
 // @description  Widen wikia pages
+// @url			 https://github.com/sordidfellow/WikiaCosmeticFix/raw/master/Widen%20Wikia%20Articles%20to%20match%20page%20width.user.js
 // @author       Me
 // @match        *.wikia.com/*
 // @grant        GM_addStyle
 // ==/UserScript==
 
 GM_addStyle("#WikiaMainContent { width: 100% !important }");
-
