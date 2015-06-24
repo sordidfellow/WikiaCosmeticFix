37e47a78dd667da719a48d90ea11a91e74f90d30
Stephen <xjc599yddv@snkmail.com>
2015-06-24 14:31:12 -0500

    Change namespace as tampermonkey considers it the homepage

--- Widen Wikia Articles to match page width.user.js
+++ Widen Wikia Articles to match page width.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Widen Wikia Articles to match page width
-// @namespace    http://ogletree.io/
+// @namespace    https://github.com/sordidfellow/WikiaCosmeticFix/
 // @version      1.0
 // @description  Widen wikia pages
 // @url          https://github.com/sordidfellow/WikiaCosmeticFix/raw/master/Widen%20Wikia%20Articles%20to%20match%20page%20width.user.js
