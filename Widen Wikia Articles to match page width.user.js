5ea9ed6b8cfdb50b8bdd89631930d66649e809ed
SordidFellow <xjc599yddv@snkmail.com>
2015-06-24 14:23:59 -0500

    Initial Version of userscript

---
+++ Widen Wikia Articles to match page width.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Widen Wikia Articles to match page width
+// @namespace    http://ogletree.io/
+// @version      0.1
+// @description  Widen wikia pages
+// @author       Me
+// @match        *.wikia.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle("#WikiaMainContent { width: 100% !important }");
+
